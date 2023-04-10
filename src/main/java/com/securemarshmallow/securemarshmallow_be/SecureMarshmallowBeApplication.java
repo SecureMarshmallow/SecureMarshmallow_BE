a710bf3b581a41863efdbfbec5acc30473d9b561
junha <goodjunha@gmail.com>
2023-04-10 14:02:40 +0900

    📦:: 프로젝트 세팅

---
+++ src/main/java/com/securemarshmallow/securemarshmallow_be/SecureMarshmallowBeApplication.java
@@ -0,0 +1,13 @@
+package com.securemarshmallow.securemarshmallow_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureMarshmallowBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureMarshmallowBeApplication.class, args);
+	}
+
+}
